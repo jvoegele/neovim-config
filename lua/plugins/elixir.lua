2083ff3a38e41079b65adc49f389cf8af6664b10
Jason Voegele <jason.voegele@gmail.com>
2023-08-23 12:34:16 -0400

    Add Elixir config as per LazyVim docs
    https://www.lazyvim.org/extras/lang/elixir

---
+++ lua/plugins/elixir.lua
@@ -0,0 +1,46 @@
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "plugins" },
+  },
+})
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "elixir",
+        "heex",
+        "eex",
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "elixir-ls",
+      })
+    end,
+  },
+
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "jfpedroza/neotest-elixir",
+    },
+    opts = {
+      adapters = {
+        ["neotest-elixir"] = {},
+      },
+    },
+  },
+
+  {
+    "jfpedroza/neotest-elixir",
+  },
+}
